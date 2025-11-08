414491bf8681abaf3e267289ee358fef11c68482
NotIvny <125482125+NotIvny@users.noreply.github.com>
2025-11-09 00:43:39 +0800

    new Code

--- debugger_example/main.cpp
+++ debugger_example/main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
+#include "search.h"
+#include "camera.h"
 using namespace std;
 int main(int argc, char* argv[]) {
-    //上位机发送调试命令会传两个参数 startIndex 和 endIndex
     for (int i = 1; i < argc; i++) {
         string arg = argv[i];
         if (arg == "--startIndex" && i + 1 < argc) {
@@ -19,15 +20,17 @@ int main(int argc, char* argv[]) {
     for(;image_process_start_idx <= image_process_end_idx; image_process_start_idx++){
         try {
             //读取图片
+            //grayFrame = cv::imread("/home/smartcar/图传上位机/image/" + to_string(image_process_start_idx) + ".jpg");
             ReadImage();
         } catch (const cv::Exception& e) {
             continue;
         }
-        //图像处理函数，和小车上运行的图像处理函数应该是一样的
+        //图像处理函数
         Search(); 
         //给图像上画边界点
         showFrame();
         //输出图片到文件夹中
+        //const string outputPath = "/home/smartcar/output/" + to_string(image_process_start_idx) + ".jpg";
         OutputImage();
     }
     return 0;
